327fc302d3667c80e6761c801e439e634f7fc3aa
Ayas Ibrahim <aayasabraham@gmail.com>
2026-05-23 02:03:40 +0700

    fix: correct server running message and clean up code formatting

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,22 @@
-import express, {
-  type Application,
-  type Request,
-  type Response,
-} from "express";
+import express, { type Application, type Request, type Response } from "express";
 import globalErrorHandler from "./middleware/globelErrorHandler";
 import { userRoute } from "./modules/auth/auth.route";
 import { issuesRoute } from "./modules/issues/issues.route";
+
 const app: Application = express();
+
 app.use(express.json());
+
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
-    message: "DevPulse Server Runing",
+    message: "DevPulse Server Running",
     author: "Ayas Ibrahim",
   });
 });
+
 app.use("/api/auth", userRoute);
 app.use("/api/issues", issuesRoute);
 
 app.use(globalErrorHandler);
+
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,19 @@
+// import app from "./app";
+// import config from "./config";
+// import { initDB } from "./db";
+
+
+// const main = () => {
+//   initDB()
+//   app.listen(config.port, () => {
+//     console.log(`App listening on port ${config.port}`);
+//   });
+// };
+// main();
+
 import app from "./app";
-import config from "./config";
 import { initDB } from "./db";
 
+initDB();
 
-const main = () => {
-  initDB()
-  app.listen(config.port, () => {
-    console.log(`App listening on port ${config.port}`);
-  });
-};
-main();
+export default app;
